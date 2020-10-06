cbfc5ff24820dd18000afb27abb0a0d75e491be5
SwethaChandru <cswethachandru@gmail.com>
2020-10-06 23:42:11 +0530

    bcryptjs installed

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const User=require('../Models/userModel');
 const jwt=require('jsonwebtoken');
 var nodemailer = require('nodemailer');
