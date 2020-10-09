137b6a9916080520e91f660021d6c4af9242d07e
SwethaChandru <cswethachandru@gmail.com>
2020-10-09 21:35:20 +0530

    initialcommit

--- Routes/taskRoute.js
+++ Routes/taskRoute.js
@@ -11,9 +11,9 @@ router.delete('/delete/:id',taskCon.deletetask);
 router.get('/:id',taskCon.getTaskByAdmin)
 router.get('/usertask/:username',taskCon.getTaskByuser);
 router.get("/edit/:id",taskCon.getTaskForEdit);
-router.get('/:date/:id',taskCon.getTaskByDate);
-router.get('/user/:date/:uname',taskCon.UserTaskByDate);
-router.get("/filter/:date/:value/:id",taskCon.filter)
-router.get("/filteruser/:date/:value/:uname",taskCon.userfilter);
+// router.get('/:date/:id',taskCon.getTaskByDate);
+// router.get('/user/:date/:uname',taskCon.UserTaskByDate);
+// router.get("/filter/:date/:value/:id",taskCon.filter)
+// router.get("/filteruser/:date/:value/:uname",taskCon.userfilter);
 
 module.exports=router;
